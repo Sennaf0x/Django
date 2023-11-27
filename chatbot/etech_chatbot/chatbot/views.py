75e41ccde29e9fffd25b80eb2f99ebfcb963caaf
Sennaf0x <Sennafo@hotmail.com>
2023-11-26 23:18:34 -0400

    Integrando o GPT-4

--- chatbot/etech_chatbot/chatbot/views.py
+++ chatbot/etech_chatbot/chatbot/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+#import dotenv
 
+openai_api_key = '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'
+openai.api_key = openai_api_key
+#env_vars = dotenv.dotenv_values()
+#openai.api_key = env_vars['OPENAI_SECRET_KEY']
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    
+    answer = response.choices[0].message.content.strip()
+    return answer
+ 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
